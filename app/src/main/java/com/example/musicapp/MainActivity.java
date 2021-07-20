2a11cd1e28da3a2e7cc7a0fc910567f541ec7845
sagivlugasi <sagivapps@gmail.com>
2021-07-20 11:55:08 +0300

    merge func

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -2,67 +2,68 @@ package com.example.musicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.SequenceInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView status;
+    static final String path ="src/main/res/raw/";
+    public File merged =new File(path,"merged.mp3");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        File song1 = null;
-        File song2 = null;
-        File merged = null;
 
-        mergeSongs(merged,song1,song2);
+        status = findViewById(R.id.status);
+        status.setText(" ");
 
-
-
-    }
-    public void mergeSongs(File mergedFile,File...mp3Files){
-        FileInputStream fisToFinal = null;
-        FileOutputStream fos = null;
         try {
-            fos = new FileOutputStream(mergedFile);
-            fisToFinal = new FileInputStream(mergedFile);
-            for(File mp3File:mp3Files){
-                if(!mp3File.exists())
-                    continue;
-                FileInputStream fisSong = new FileInputStream(mp3File);
-                SequenceInputStream sis = new SequenceInputStream(fisToFinal, fisSong);
-                byte[] buf = new byte[1024];
-                try {
-                    for (int readNum; (readNum = fisSong.read(buf)) != -1;)
-                        fos.write(buf, 0, readNum);
-                } finally {
-                    if(fisSong!=null){
-                        fisSong.close();
-                    }
-                    if(sis!=null){
-                        sis.close();
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally{
-            try {
-                if(fos!=null){
-                    fos.flush();
-                    fos.close();
-                }
-                if(fisToFinal!=null){
-                    fisToFinal.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            File sdcard = Environment.getExternalStorageDirectory();
+            File song1 = new File(path,"bpm180.mp3");
+            File song2 =  new File(path,"song1.mp3");
+
+            mergeSongs(merged,song1,song2);
+
+          //  MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.merged);
+          //  mediaPlayer.start();
+          //  wait(3000);
+          //  mediaPlayer.stop();
+            Toast.makeText(MainActivity.this,"WORK",Toast.LENGTH_SHORT).show();
+            status.setText("Its working");
+
         }
+        catch (Exception e)
+        {
+            Toast.makeText(MainActivity.this,"dosent work",Toast.LENGTH_SHORT).show();
+            status.setText("Dosent work");
+
+        }
+
     }
+    public void mergeSongs(File mergedFile,File song1, File song2) throws IOException {
+        byte[] byte1 = Files.readAllBytes(Paths.get("/bpm180.mp3"));
+        byte[] byte2 = Files.readAllBytes(Paths.get("/song1.mp3"));
+        byte[] out = new byte[byte1.length];
+        for (int i=0; i<byte1.length; i++) {
+            out[i] = (byte) ((byte1[i] + byte2[i]) >> 1);
+        }
+
+    }
+
+
+
 }
