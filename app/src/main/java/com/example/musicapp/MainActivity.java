6c9cfe3e7a2d177cc451017c3ee7b0eb61ca7184
sagivlugasi <sagivapps@gmail.com>
2021-07-29 14:19:14 +0300

    tic tac toe

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -3,31 +3,87 @@ package com.example.musicapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     static int c = 1;
+    int [] [] game = {{0,0,0},
+                      {0,0,0},
+                      {0,0,0}};
+    int player = 0;
+    boolean gameOver = false;
 
     public void DropIn(View view)
     {
-        ImageView counter = (ImageView)  view;
-        counter.setTranslationY(-1000f);
-        if(c % 2 == 0) {
-            counter.setImageResource(R.drawable.fcb);
-        }
-        else
-        {
-            counter.setImageResource(R.drawable.arg);
-        }
-        counter.animate().translationYBy(1000f).setDuration(300);
-        c++;
+        if (!gameOver) {
+            ImageView counter = (ImageView) view;
+            if (counter.getDrawable() == null) {
+                counter.setTranslationY(-1000f);
+                if (c % 2 == 0) {
+                    counter.setImageResource(R.drawable.fcb);
+                    player = 1;
+                } else {
+                    counter.setImageResource(R.drawable.arg);
+                    player = 2;
+                }
+                counter.animate().translationYBy(1000f).setDuration(300);
+                c++;
 
+                int x = Integer.parseInt(String.valueOf(counter.getTag().toString().charAt(0)));
+                int y = Integer.parseInt(String.valueOf(counter.getTag().toString().charAt(2)));
+                game[x][y] = player;
+
+                for (int i = 0; i < 3; i++) {
+                    Log.i("array", String.valueOf((game[i][0]) + "  " + (game[i][1]) + " " + game[i][2]));
+                }
+                Log.i("array", "________________________");
+
+                if (c > 5) {
+                    for (int i = 0; i < 3; i++) {
+                        if (game[i][0] == game[i][1] && game[i][0] == game[i][1] && game[i][0]!=0 ) {
+                            gameOver = true;
+                            player = game[i][0];
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < 3; i++) {
+                        if (game[0][i] == game[1][i] && game[1][i] == game[2][i] && game[i][0]!=0)  {
+                            gameOver = true;
+                            player = game[0][i];
+                            break;
+                        }
+                    }
+                    if( (game[0][0] == game[1][1] &&game[1][1] == game[2][2] )   ||
+                            (game[2][0] == game[1][1] &&game[1][1] == game[0][2])  )
+                    {
+                        gameOver = true;
+                        player = game[1][1];
+                    }
+                    if (gameOver && player == 2)
+                    {
+                        Toast.makeText(MainActivity.this, "Argentina won!",Toast.LENGTH_LONG).show();
+                        c = 1;
+                        Log.i( "","Game Over");
+                    }
+                    else if (gameOver)
+                    {
+                        Toast.makeText(MainActivity.this, "Barcelona won!",Toast.LENGTH_LONG).show();
+                        c = 1;
+                        Log.i( "","Game Over");
+
+                    }
+
+
+                }
+            }
+        }
 
     }
 
-    ImageView i1,i2,i3,i4,i5,i6,i7,i8,i9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
